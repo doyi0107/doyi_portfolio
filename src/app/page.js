eabec56c78ad768160d227e491cdf6fe4bae82e7
doyi <doyi9917@naver.com>
2023-12-01 19:13:33 +0900

    sub page complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 "use client";
 import "./page.modul.css";
+import Image from "next/image";
+import networking from "/public/networking.png";
+import design from "/public/design.png";
+import watering from "/public/watering.png";
 // import gsap from "gsap";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import LocomotiveScroll from "locomotive-scroll";
@@ -10,12 +14,43 @@ export default function Home() {
     <>
       <div className="container">
         <section id="main_sub">
-          <h2>main_Sub</h2>
+          {/*  */}
           <div className="sub_box">박스</div>
+          {/* who is */}
+          <h2 className="sub_name">Who is DOYI?</h2>
+          {/* #소통 감각 성장 */}
           <div className="explain_me">
-            <div  className="one">#소통하는</div>
-            <div  className="two">#감각있는</div>
-            <div  className="three">#성장하는</div>
+            <div className="one img_wrap">
+              <Image
+                className="networking sub_img"
+                src={networking}
+                alt="networking"
+              />
+              <h3>#소통하는</h3>
+              <p className="net_exp sub_text">
+                사람들과 하나의 목표를 향해 의견을 공유하며 나아가는 활동을
+                좋아합니다.
+              </p>
+            </div>
+            <div className="two img_wrap">
+              <Image className="design sub_img" src={design} alt="design" />
+              <h3>#감각있는</h3>
+              <p className="design_exp sub_text">
+                상상한 그림을 디자인하고 제작하는 작업에 큰 성취감을 느낍니다. 
+              </p>
+            </div>
+            <div className="three img_wrap">
+              <Image
+                className="watering sub_img"
+                src={watering}
+                alt="watering"
+              />
+              <h3>#성장하는</h3>
+              <p className="watering_exp sub_text">
+                나를 알고 부족한 부분을 채워가기 위해 반복과 복습을 습관처럼
+                하고 있습니다.
+              </p>
+            </div>
           </div>
         </section>
 
