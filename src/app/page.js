642e1461e276f71cba682d604db99824f21fbda2
doyi <doyi9917@naver.com>
2023-12-08 20:26:28 +0900

    반응형complete

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,16 @@ import t_story from "/public/t-story.png";
 import APMA from "/public/APMA_img.png";
 import source_code from "/public/source-code.png";
 import demo from "/public/demo.png";
+import talk from "/public/talk.png";
+import mail from "/public/email.png";
+import phone from "/public/phone.png";
+import instagram from "/public/instagram.png";
+import home from "/public/home.png";
+import birth from "/public/birth.png";
+import academic from "/public/academic.png";
+import name from "/public/name.png";
+import footer_github from "/public/footer_github.png";
+import footer_t_story from "/public/footer_t_story.png";
 import contact_doyi from "/public/contact_doyi.jpg";
 import Link from "next/link";
 import gsap from "gsap";
@@ -20,6 +30,27 @@ export default function Home() {
   const [isSpecialCase, setIsSpecialCase] = useState(false);
   const scrollSectionRef = useRef(null);
   const scrollContentRef = useRef(null);
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleScrollGuideClick = () => {
+    // Check if the className is "scroll_guide_up" before applying the function
+    if (isSpecialCase) {
+      // Your function here
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // You can change this to 'auto' for instant scrolling
+      });
+    }
+  };
 
   useEffect(() => {
     const scrollSection = scrollSectionRef.current;
@@ -65,7 +96,10 @@ export default function Home() {
 
   return (
     <>
-      <div className={`${isSpecialCase ? "scroll_guide_up" : "scroll_guide"}`}>
+      <div
+        className={`${isSpecialCase ? "scroll_guide_up" : "scroll_guide"}`}
+        onClick={handleScrollGuideClick}
+      >
         <span></span>
         <span></span>
         <span></span>
@@ -276,44 +310,121 @@ export default function Home() {
                 className="contact_doyi_img"
                 src={contact_doyi}
                 alt="contact_doyi"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
               />
-              <div className="contact_info_doyi">
-                <dl>
-                  <dt>이름:</dt>
-                  <dd>이도이</dd>
-                </dl>
-                <dl>
-                  <dt>생년월일:</dt>
-                  <dd>1999.01.07</dd>
-                </dl>
-                <dl>
-                  <dt>주소지:</dt>
-                  <dd>인천광역시 미추홀구</dd>
-                </dl>
-                <dl>
-                  <dt>학력:</dt>
-                  <dd>
-                    국민대(국어국문학과(주)/소프트웨어미디어융합전공(부))
-                  </dd>
-                </dl>
+            </div>
+            <div className={isHovered ? "display_none" : "contact_text_wrap"}>
+              <div className="contact_text_box">
+                <div className="contact_text_title">
+                  <Image className="phone" src={phone} alt="phone" />
+                  <h3>전화번호</h3>
+                </div>
+                <p>010-7277-7361</p>
+                <div className="contact_text_title">
+                  <Image className="mail" src={mail} alt="mail" />
+                  <h3>이메일</h3>
+                </div>
+                <p>doyi9917@naver.com</p>
+                <div className="contact_text_title">
+                  <Image className="talk" src={talk} alt="talk" />
+                  <h3>카카오톡</h3>
+                </div>
+                <Link href="https://open.kakao.com/o/sevgyHWf" target="blank">
+                  <p>→Doyi9917</p>
+                </Link>
+                <div className="contact_text_title">
+                  <Image
+                    className="instagram"
+                    src={instagram}
+                    alt="instagram"
+                  />
+                  <h3>인스타그램</h3>
+                </div>
+                <Link
+                  href="https://instagram.com/2_doooo_2?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr"
+                  target="blank"
+                >
+                  <p>→2_doooo_2</p>
+                </Link>
               </div>
             </div>
-            <div className="contact_text_wrap">
+            <div className={isHovered ? "contact_info_wrap" : "display_none"}>
               <div className="contact_text_box">
-                <h3>전화번호</h3>
-                <p>010-7277-7361</p>
-                <h3>이메일</h3>
-                <p>doyi9917@naver.com</p>
-                <h3>카카오톡</h3>
-                <p>doyi9917@naver.com</p>
-                <h3>인스타그램</h3>
-                <p>doyi9917@naver.com</p>
+                <div className="contact_text_title">
+                  <Image className="name" src={name} alt="name" />
+                  <h3>이름</h3>
+                </div>
+                <p>이도이</p>
+                <div className="contact_text_title">
+                  <Image className="birth" src={birth} alt="birth" />
+                  <h3>생년월일</h3>
+                </div>
+                <p>1999.01.07</p>
+                <div className="contact_text_title">
+                  <Image className="academic" src={academic} alt="academic" />
+                  <h3>학력</h3>
+                </div>
+                <p>
+                  국민대(국어국문학과(주)/
+                  <br />
+                  소프트웨어미디어융합전공(부))
+                </p>
+                <div className="contact_text_title">
+                  <Image className="home" src={home} alt="home" />
+                  <h3>거주지</h3>
+                </div>
+                <p>인천 미추홀구</p>
               </div>
             </div>
           </div>
         </section>
+
         <footer>
-          <p>꽃을 피울 개발자로 성장할 도이가 궁금하다면 연라주세요</p>
+          <p className="footer_Copyright">Copyright © 2024 Leedoyi</p>
+          <p className="footer_mail">
+            꽃을 피울 개발자 이도이가 궁금하다면 메일을 보내주세요 →
+            doyi9917@naver.com
+          </p>
+
+          <div className="footer_link_wrap">
+            <Link
+              href="https://github.com/doyi0107"
+              className="footer_link_github footer_link"
+              target="blank"
+            >
+              <Image
+                className="footer_github"
+                src={footer_github}
+                alt="footer_github"
+              />
+            </Link>
+            <Link
+              href="https://2-doooo-2.tistory.com/"
+              target="blank"
+              className="footer_link_tstory footer_link"
+            >
+              <Image
+                className="footer_t_story"
+                src={footer_t_story}
+                alt="footer_t_story"
+              />
+            </Link>
+            <Link
+              href="https://open.kakao.com/o/sevgyHWf"
+              target="blank"
+              className="footer_link_kakao footer_link"
+            >
+              <Image className="talk" src={talk} alt="talk" />
+            </Link>
+            <Link
+              href="https://instagram.com/2_doooo_2?igshid=MzMyNGUyNmU2YQ%3D%3D&utm_source=qr"
+              target="blank"
+              className="footer_link_kakao footer_link"
+            >
+              <Image className="instagram" src={instagram} alt="instagram" />
+            </Link>
+          </div>
         </footer>
       </div>
     </>
