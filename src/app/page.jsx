bd326bf5b4fa6c66aca13bbda5a142ed60c8dfe8
doyi0107 <doyi9917@naver.com>
2024-10-27 20:24:26 +0900

    스크롤 이벤트 패

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,46 +10,54 @@ import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
 
 export default function Home() {
-  const [isSpecialCase, setIsSpecialCase] = useState(false);
-  const containerRef = useRef(null);
+    const [isSpecialCase, setIsSpecialCase] = useState(false);
+    const containerRef = useRef(null);
 
-  const handleScrollGuideClick = () => {
-    if (isSpecialCase) {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
- 
-      if (containerRef.current) {
-        
-          const isScrollAtBottom =
-            window.innerHeight + window.scrollY >= document.body.offsetHeight;
-          const scrollTop = window.scrollY;
-
-          // 상단으로 스크롤될 때의 조건을 설정 (예: 0 또는 원하는 값)
-          const isTop = scrollTop === 0;
-     
-
-         if (isScrollAtBottom) {
-            setIsSpecialCase(!isSpecialCase);
-          } else if (isTop) {
-            setIsSpecialCase(isSpecialCase);
-          }
-
+    const handleScrollGuideClick = () => {
+      if (isSpecialCase) {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
       }
     };
-    const container = containerRef.current;
-    container.addEventListener("scroll", handleScroll);
 
-    return () => {
-      if (container) container.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (containerRef.current) {
+          const { scrollTop, scrollHeight } = containerRef.current;
+          const clientHeight = containerRef.current.clientHeight; // 여기서 clientHeight를 매번 가져옵니다.
+
+          console.log("scrollTop:", scrollTop);
+          console.log("scrollHeight:", scrollHeight);
+          console.log("clientHeight:", clientHeight);
+
+          // 컨테이너의 스크롤이 하단에 도달했는지 확인
+          const isScrollAtBottom = scrollTop + clientHeight >= scrollHeight;
+          console.log(isScrollAtBottom);
+
+          const isTop = scrollTop === 0;
+          console.log(isTop);
+
+          if (isScrollAtBottom) {
+            setIsSpecialCase(true); // 조건을 수정하여 true로 설정
+          } else if (isTop) {
+            setIsSpecialCase(false); // 조건을 수정하여 false로 설정
+          }
+        }
+      };
+
+      const container = containerRef.current;
+      // Scroll 이벤트를 window에 추가
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        // Cleanup: Scroll 이벤트 리스너 제거
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [containerRef.current]);
+
 
   return (
     <>
