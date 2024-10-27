7ad3a1a0ba9d43cb297c5a8f749b6ba8f59867a6
doyi0107 <doyi9917@naver.com>
2024-10-27 21:51:32 +0900

    스크롤 유도 가이드 에러 해결

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,54 +10,39 @@ import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
 
 export default function Home() {
-    const [isSpecialCase, setIsSpecialCase] = useState(false);
-    const containerRef = useRef(null);
+  const [isSpecialCase, setIsSpecialCase] = useState(false);
+  const containerRef = useRef(null);
 
-    const handleScrollGuideClick = () => {
-      if (isSpecialCase) {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
+  const handleScrollGuideClick = () => {
+    if (isSpecialCase) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+
+        const { scrollTop, scrollHeight} = containerRef.current;
+        const isScrollAtBottom = scrollTop + (window.scrollY+1) >= scrollHeight;
+        console.log(scrollTop, scrollHeight, window.scrollY);
+        
+        const isTop = window.scrollY;
+
+
+        if (isScrollAtBottom) setIsSpecialCase(true);
+        else if (isTop) setIsSpecialCase(false);
       }
-    };
 
-    // useEffect
-    useEffect(() => {
-      const handleScroll = () => {
-        if (containerRef.current) {
-          const { scrollTop, scrollHeight } = containerRef.current;
-          const clientHeight = containerRef.current.clientHeight; // 여기서 clientHeight를 매번 가져옵니다.
-
-          console.log("scrollTop:", scrollTop);
-          console.log("scrollHeight:", scrollHeight);
-          console.log("clientHeight:", clientHeight);
-
-          // 컨테이너의 스크롤이 하단에 도달했는지 확인
-          const isScrollAtBottom = scrollTop + clientHeight >= scrollHeight;
-          console.log(isScrollAtBottom);
-
-          const isTop = scrollTop === 0;
-          console.log(isTop);
-
-          if (isScrollAtBottom) {
-            setIsSpecialCase(true); // 조건을 수정하여 true로 설정
-          } else if (isTop) {
-            setIsSpecialCase(false); // 조건을 수정하여 false로 설정
-          }
-        }
-      };
-
-      const container = containerRef.current;
-      // Scroll 이벤트를 window에 추가
       window.addEventListener("scroll", handleScroll);
 
       return () => {
-        // Cleanup: Scroll 이벤트 리스너 제거
         window.removeEventListener("scroll", handleScroll);
       };
-    }, [containerRef.current]);
 
+  }, []);
 
   return (
     <>
@@ -68,7 +53,6 @@ export default function Home() {
         className={`${isSpecialCase ? "scroll_guide_up" : "scroll_guide"}`}
         onClick={handleScrollGuideClick}
       >
-        <span></span>
         <span></span>
         <span></span>
       </div>
